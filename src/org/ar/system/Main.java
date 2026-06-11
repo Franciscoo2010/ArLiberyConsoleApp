12c7e98b36cbaca9b7121353b859c63a17ad1673
AndyAjiatas07 <rpico-2023564@kinal.edu.gt>
2026-06-11 12:00:03 -0600

    001 Se agrego Clases Conexion y MenuPrincipal, edito la clase Main

--- src/org/ar/system/Main.java
+++ src/org/ar/system/Main.java
@@ -1,21 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package org.ar.system;
-
+ 
+import org.ar.view.MenuPrincipal;
+ 
 /**
- *
- * @author informatica
- */
+*
+* @author informatica
+*/
 public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
+ 
+ 
     public static void main(String[] args) {
-        // TODO code application logi
-          
+        MenuPrincipal menu = new MenuPrincipal();
+        menu.iniciar();
     }
-    
+
 }
