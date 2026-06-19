8e8955ece832581778143f96fd0410a1d4ed1248
Alexis Diéguez <a.dieguez-2023463@kinal.edu.gt>
2026-06-19 12:03:23 -0600

    feat(Clientes) se agrego la interfaz DAOImpl para Cliente, ClienteDAOImpl.java #6

---
+++ src/org/ar/dao/imp/ClienteDAOImpl.java
@@ -0,0 +1,40 @@
+
+package org.ar.dao.imp;
+
+import org.ar.model.Clientes;
+import otg.ar.dao.ClienteDAO;
+
+import java.util.List;
+
+public class ClienteDAOImpl implements ClienteDAO{
+
+    @Override
+    public boolean insertar(Clientes cliente) {
+        return false;
+    }
+
+    @Override
+    public List<Clientes> listar() {
+        return null;
+    }
+
+    @Override
+    public Clientes buscar(long cui) {
+        return null;
+    }
+
+    @Override
+    public boolean actualizar(Clientes cliente) {
+        return false;
+    }
+
+    @Override
+    public boolean eliminar(long cui) {
+        return false;
+    }
+
+    
+   
+    
+    
+}
