0ea829300bc7c3d642d3c736b80321c48d4784dc
Alexis Diéguez <a.dieguez-2023463@kinal.edu.gt>
2026-06-19 11:02:41 -0600

    feat(cliente) se agrego la clase POJO para Cliente #2

---
+++ src/org/ar/model/Clientes.java
@@ -0,0 +1,65 @@
+package org.ar.model;
+
+public class Clientes {
+
+    /*
+    cui bigint primary key,
+    nombre_cliente varchar(100),
+    apellido_cliente varchar(100),
+    correo_electronico varchar(100)
+);
+     */
+
+    // atributos dde clase
+    private long cui;
+    protected String nombre;
+    String apellido;
+    String correoElectronico;
+
+    //constructores
+public Clientes(long cui, String nombre, String apellido, String correoElectronico) {
+        this.cui = cui;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correoElectronico = correoElectronico;
+}
+    //getter and setters
+
+    public long getCui() {
+        return cui;
+    }
+
+    public void setCui(long cui) {
+        this.cui = cui;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        //formatear matuscula
+        String nombreMayuscula = nombre.toUpperCase();
+        //formatear a Iniciar con Mayúscila
+        this.nombre = nombreMayuscula;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+}
+
+    
+
