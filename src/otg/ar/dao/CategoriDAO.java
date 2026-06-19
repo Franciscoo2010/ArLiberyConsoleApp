7ee84076b3d37a384b785e049dff958c0507d08e
Informatica <rpico-2023564@kinal.edu.gt>
2026-06-19 11:41:38 -0600

    feat(categoria) se agrego la interfaz DAD para Categoria, CategoriaDAO.java #23

---
+++ src/otg/ar/dao/CategoriDAO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package otg.ar.dao;
+
+/**
+ *
+ * @author informatica
+ */
+public interface CategoriDAO {
+    
+}
