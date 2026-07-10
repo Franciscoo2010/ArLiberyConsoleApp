ca81f1fb2a39ebc15abe48e4602ccf87a21a73d6
Alexis Diéguez <a.dieguez-2023463@kinal.edu.gt>
2026-07-10 08:41:42 -0600

    feat(clientes) se añadieron cambios al controller de clientes #7

--- src/org/ar/controller/ClienteController.java
+++ src/org/ar/controller/ClienteController.java
@@ -1,6 +1,29 @@
 
 package org.ar.controller;
 
+import org.ar.dao.imp.ClienteDAOImpl;
+import org.ar.view.ClienteConsoleView;
+import otg.ar.dao.ClienteDAO;
+
 public class ClienteController {
+    private final ClienteDAO dao;
+    private final ClienteConsoleView vista;
     
+    public ClienteController(ClienteConsoleView vista){
+        this.dao = new ClienteDAOImpl();
+        this.vista = vista;
+    }
+    
+    public void iniciar(){
+        int opcion;
+        do{
+            opcion = vista.mostrarMenu();
+            if(opcion == 2){
+                listar();
+            }
+        } while (opcion !=  4);
+    }
+    private void listar(){
+        vista.mostrarListaClientes( dao.listarTodos());
+    }
 }
