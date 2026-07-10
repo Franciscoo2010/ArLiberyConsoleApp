22831f7ae8c6c478ca10802e39ada62c9d0dbfde
Informatica <rpico-2023564@kinal.edu.gt>
2026-07-10 11:19:30 -0600

    feat(categorias) se añadieron cambios en la clase  controller de categoria #25

--- src/org/ar/controller/CategoriaController.java
+++ src/org/ar/controller/CategoriaController.java
@@ -1,13 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.ar.controller;
 
-/**
- *
- * @author informatica
- */
+import org.ar.dao.imp.CategoriaDAOImpl;
+
+import org.ar.model.Categoria;
+import org.ar.view.CategoriaConseleView;
+import otg.ar.dao.CategoriDAO;
+ 
 public class CategoriaController {
+    private final CategoriDAO dao; 
+    private final CategoriaConseleView vista;
+    public CategoriaController(CategoriaConseleView vista) { 
+    this.dao = new CategoriaDAOImpl();
+    this.vista = vista; 
+}
+    public void iniciar(){
+        int opcion ;
+        do {
+            opcion = vista.mostrarMenu(); 
+            switch (opcion ) {
+                case 1 : 
+                    break ; 
+                case 2: 
+                    listar(); 
+                break; 
+                case 3: 
+                    buscar(); 
+                    break;
+                    case 4: 
+            }
+        }while (opcion != 0) ; 
+    } 
+    private void listar(){ 
+        vista.mostrarlistaCategoria(dao.listarTodos());
+    }
+ 
+    private void buscar() {
+        int id = vista.solicitarIdCategoria(); 
+        Categoria categoria = dao.buscarPorId(id);
+        if (categoria != null ) {
+            vista.mostrarCategoria(categoria);
+        }else {
+            vista.mostrarMensaje("Cliente  no encontrado con el ID  " + id); 
+        }
     
+    }
 }
