27c6d4846b061e6593d94c1c83e22a4c1180d204
Informatica <rpico-2023564@kinal.edu.gt>
2026-07-10 07:53:33 -0600

    feat(cliente) se agrego el archivo de vista, se completo #22

---
+++ src/org/ar/view/CategoriaConseleView.java
@@ -0,0 +1,72 @@
+
+package org.ar.view;
+import java.util.Scanner;
+import java.util.List;
+import org.ar.model.Categoria;
+public class CategoriaConseleView {
+  private final Scanner leer = new Scanner(System.in);
+    
+    //metodo para mostrar las opciones de este menú
+    public int mostrarMenu(){
+        int opcion = 0;
+        
+        System.out.println("-------Gestion de Clientes---------");
+        System.out.println("-1 CREAR NUEVO CLIENTE-");
+        System.out.println("-2 LISTAR CLIENTES-");
+        System.out.println("-3 BUSCAR CLIENTE POR ID-");
+        System.out.println("-4 MODIFICAR CLIENTE-");
+        System.out.println("-5 ELIMINAR CLIENTE-");
+        System.out.println("-6 REGRESAR MENU PRINCIPAL-");
+        System.out.println("-Seleccione una opción-");
+        opcion = Integer.parseInt(leer.nextLine());
+        return opcion;
+    }
+    public long solicitarCUI(){
+        System.out.println("ingrese el CUI del cliente");
+        return Long.parseLong(leer.nextLine());
+    }
+    
+    public String solicitarNombreCliente(){
+        System.out.println("ingrese el Nombre del cliente");
+        String nombre;
+        nombre = leer.nextLine();
+        return nombre;
+        //otra forma de hacerlo
+    }
+    
+    public String solicitarApellidoCliente(){
+        System.out.println("ingrese el Apellido del cliente");
+        return leer.nextLine();
+    }
+    
+    public String solicitarCorreoElectronico(){
+        System.out.println("ingrese el correo electronico");
+        return leer.nextLine();
+    }
+    
+    //mostrar el detalle de un CLIENTE
+    public void mostrarCliente(Categoria categoria){
+        System.out.println("--- DATOS DEL CLIENTE ---");
+        System.out.println("CUI: " + categoria.getCui());
+        System.out.println("NOMBRE: " + categoria.getNombre());
+        System.out.println("APELLIDO: " + categoria.getApellido());
+        System.out.println("CORREO: " + categoria.getCorreoElectronico());
+    }
+    //mostrar la lista de CLIENTES -- lista de objeto List<T>, ArrayList<Cliente>
+    public void mostrarListaClientes(List<Categoria> clientes){
+        System.out.println("--LISTA DE CLIENTES ---");
+        //tabla usando la propidad %- tamaño de columtas
+        System.out.println("|CUI   |   NOMBRE  |   APELLIDO   |  CORREO ELECTRÓNICO  |");
+        System.out.printf("%-10s %-10s %-10s %-10s", "CUI","NOMBRE","APELLIDO","CORREO");
+        
+        for (Categoria categoria : clientes) {
+                    System.out.printf("%-10s %-10s %-10s %-10s", 
+                            categoria.getCui(), categoria.getNombre(), categoria.getApellido(), categoria.getCorreoElectronico());
+
+        }
+    }
+    public void mostrarMensaje(String mensaje){
+        System.out.println(mensaje);
+    }
+    
+}
