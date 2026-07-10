b4d9edb2b6169fa73d418314ba82d1613c655477
Alexis Diéguez <a.dieguez-2023463@kinal.edu.gt>
2026-07-10 08:32:08 -0600

    feat(cliente) se agregaron cambios al Menu Principal de clientes #9

--- src/org/ar/view/MenuPrincipal.java
+++ src/org/ar/view/MenuPrincipal.java
@@ -1,13 +1,72 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package org.ar.view;
 
-/**
- *
- * @author informatica
- */
+import java.util.Scanner;
+import org.ar.controller.ClienteController;
+import org.ar.view.ClienteConsoleView;
+
 public class MenuPrincipal {
+    private final Scanner leer = new Scanner(System.in);
     
+    //heramienta scanner: lee datos del usuario
+    
+    public void iniciarSistema(){
+        int opcion;
+        //ciclo para el menu: do while
+        do {            
+            System.out.println("--------------------------------------");
+            System.out.println("    SISTEMA CENTRAL LIBRERIA - IN4CM");
+            System.out.println("--------------------------------------");
+            System.out.println("1. Entrar a CLIENTES");
+            System.out.println("2. Entrada a CATEGORIAS");
+            System.out.println("3. Entrada a LIBROS");
+            System.out.println("4. Entrada a EDITORIALES");
+            System.out.println("5. Entrada a COMPRAS");
+            System.out.println("6. Entrada a AUTORES");
+            System.out.println("7. Entrada a DETALLE AUTORES");
+            System.out.println("8. SALIR DEL SISTEMA");
+            System.out.println("Selecciones una opción: ");
+            
+            opcion = Integer.parseInt(leer.nextLine());
+        //swich / case
+            switch (opcion) {
+                case 1:
+                    //instanciar la vista CLIENTES
+                    ClienteConsoleView vista = new ClienteConsoleView();
+                    ClienteController control = new ClienteController(vista);
+                    control.iniciar();
+                    break;
+                case 2:
+                    //instanciar la vista EDITORIALES
+                    System.out.println("CATEGORIAS");
+                    break;
+                case 3:
+                    //instanciar la vista LIBROS
+                    System.out.println("LIBROS");
+                    break;
+                case 4:
+                    //instanciar la vista LIBROS
+                    System.out.println("EDITORIALES");
+                    break;
+                case 5:
+                    //instanciar la vista LIBROS
+                    System.out.println("COMPRAS");
+                    break;
+                 case 6:
+                    //instanciar la vista LIBROS
+                    System.out.println("AUTORES");
+                    break;
+                case 7:
+                    //instanciar la vista LIBROS
+                    System.out.println("DETALLE AUTORES");
+                    break;
+                case 8:
+                    //instanciar la vista LIBROS
+                    System.out.println("\n Hasta luego sixseveniano...");
+                    break;
+                default:
+                    System.out.println("no existe esta opción");
+            }
+        } while (opcion != 8);
+        
+    }
 }
