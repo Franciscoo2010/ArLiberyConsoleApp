ba0ce73a415e7984f2beb7ffd023c63812ffcaad
Informatica <rpico-2023564@kinal.edu.gt>
2026-07-10 11:03:48 -0600

    000 nada

--- src/org/ar/system/Main.java
+++ src/org/ar/system/Main.java
@@ -1,19 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package org.ar.system;
 
-import org.ar.util.Conexion;
-
+/**
+ *
+ * @author informatica
+ */
 public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        //sout
-        //MVC Modelo,Vista, Controlador. Esto mejora la legibilidad y estandariza en código (patron de diseño)
-        //DAO (objeto de acceso a datos)
-        MenuPrincipal menu = new MenuPrincipal();
-        menu.iniciarSistema();
-        Conexion db = new Conexion();
-        db.conectar();
+        // TODO code application logi
+          
     }
+    
 }
--- src/org/ar/util/Conexion.java
+++ src/org/ar/util/Conexion.java
@@ -1,34 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package org.ar.util;
+ 
+ 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+ 
 public class Conexion {
-    //sString de conexion: URL, USER, PASSWORD
-    //URL : driver, host de server: puerto, db, ?config..
-    private static final String URL = 
-            "jdbc:mysql://localhost/db_DDL_Alexis_Diéguez_in4cm?useUnicode=true&characterEncoding=UTF-8";
+    private static Conexion instancia;
+    // Configuración del string de conexión, y credenciales
+    private static final String URL = "jdbc:mysql://localhost:3306/libreriadb_in4cm?serverTimezone=UTC";
     private static final String USER = "IN4CM";
     private static final String PASSWORD = "#NdimAM4";
-    public static Connection conectar(){
-        Connection conexion = null;
+ 
+    //Constructor privado para evitar que hagan "new Conexion()" fuera de esta clase
+    private Conexion() {
         try {
-            //driver de JDBC
             Class.forName("com.mysql.cj.jdbc.Driver");
-            //crearla conexión
-            conexion = DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (ClassNotFoundException e) {
-            System.err.println("ERROR: no hay driver");
-        }catch (SQLException ex){
-            System.err.println("ERROR: no se pudo conectar a la DB"+ex.getMessage());
+            System.err.println("Error Driver: " + e.getMessage());
         }
-        return conexion;
+    }
+ 
+    //Método público estático para obtener la única instancia del Gestor
+    public static synchronized Conexion getInstancia() {
+        if (instancia == null) {
+            instancia = new Conexion();
+        }
+        return instancia;
+    }
+ 
+    //Método para entregar una conexión fresca cada vez que se pida
+    public Connection conectar() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 
-    public static Object getInstancia() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
 }
