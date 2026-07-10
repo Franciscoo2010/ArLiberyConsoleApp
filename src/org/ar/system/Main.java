f774d1f27cbf6f093ade488e8e66ba71c5503b94
Andree Zabala <azabala-2023347@kinal.edu.gt>
2026-07-10 08:51:04 -0600

    se añadieron cambios al método main

--- src/org/ar/system/Main.java
+++ src/org/ar/system/Main.java
@@ -1,27 +1,15 @@
 package org.ar.system;
 
-import java.util.List;
-import org.ar.dao.imp.editorialDAOImpl;
-import org.ar.model.editorial;
+import org.ar.view.MenuPrincipal;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        editorialDAOImpl dao = new editorialDAOImpl();
+        // MVC Modelo, Vista, Controlador
+        // DAO (Objeto de Acceso a Datos)
 
-        List<editorial> lista = dao.listarTodos();
-
-        if (lista.isEmpty()) {
-            System.out.println("No hay editoriales registradas.");
-        } else {
-            for (editorial e : lista) {
-                System.out.println("NIT: " + e.getNit());
-                System.out.println("Nombre: " + e.getNombre_editorial());
-                System.out.println("Teléfono: " + e.getTelefono_editorial());
-                System.out.println("Dirección: " + e.getDireccion_editoria());
-                System.out.println("-------------------------");
-            }
-        }
+        MenuPrincipal menu = new MenuPrincipal();
+        menu.iniciarSistema();
     }
 }
