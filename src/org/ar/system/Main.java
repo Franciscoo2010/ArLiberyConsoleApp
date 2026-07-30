7f8daa1ff10bab76cfac140418718fff8ce5da31
Alexis Diéguez <adieguez-2023463@kinal.edu.gt>
2026-07-30 12:05:09 -0600

    feat(ft/login) se añadieron cambios en Main

--- src/org/ar/system/Main.java
+++ src/org/ar/system/Main.java
@@ -1,21 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package org.ar.system;
 
-/**
- *
- * @author informatica
- */
-public class Main {
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logi
-          
+public class Main extends Application {
+
+    private static Stage escenarioPrincipal;
+
+    public static void cambiarEscena(String rutaFXML) throws IOException {
+        //Parent raiz = FXMLLoader.load(getClass().getResource(rutaFXML));
+        Parent raiz = FXMLLoader.load(Main.class.getResource(rutaFXML));                
+        Scene escena = new Scene(raiz); 
+        escenarioPrincipal.setScene(escena);
+        escenarioPrincipal.sizeToScene();
+        escenarioPrincipal.centerOnScreen();
+        escenarioPrincipal.show();        
     }
-    
+
+    @Override
+    public void start(Stage escenarioPrincipal) throws Exception {
+        //convertir .fxml en nodo raiz
+        Main.escenarioPrincipal = escenarioPrincipal;     
+        cambiarEscena("/org/ac/view/InisioSesionView.fxml");
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
