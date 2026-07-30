9704e432a4b1343a6231606cf6ae093c313294d2
Informatica <rpico-2023564@kinal.edu.gt>
2026-07-30 11:39:28 -0600

    se modifico controller

--- src/org/ar/controller/CategoriaController.java
+++ src/org/ar/controller/CategoriaController.java
@@ -1,47 +1,58 @@
 package org.ar.controller;
 
 import org.ar.dao.imp.CategoriaDAOImpl;
-
 import org.ar.model.Categoria;
 import org.ar.view.CategoriaConseleView;
 import otg.ar.dao.CategoriDAO;
- 
+
 public class CategoriaController {
-    private final CategoriDAO dao; 
+
+    private final CategoriDAO dao;
     private final CategoriaConseleView vista;
-    public CategoriaController(CategoriaConseleView vista) { 
-    this.dao = new CategoriaDAOImpl();
-    this.vista = vista; 
-}
-    public void iniciar(){
-        int opcion ;
-        do {
-            opcion = vista.mostrarMenu(); 
-            switch (opcion ) {
-                case 1 : 
-                    break ; 
-                case 2: 
-                    listar(); 
-                break; 
-                case 3: 
-                    buscar(); 
-                    break;
-                    case 4: 
-            }
-        }while (opcion != 0) ; 
-    } 
-    private void listar(){ 
-        vista.mostrarlistaCategoria(dao.listarTodos());
+
+    public CategoriaController(CategoriaConseleView vista) {
+        this.dao = new CategoriaDAOImpl();
+        this.vista = vista;
     }
- 
+
+    public void iniciar() {
+        int opcion;
+        do {
+            opcion = vista.mostrarMenu();
+            switch (opcion) {
+                case 1:
+                    break;
+                case 2:
+                    listar();
+                    break;
+                case 3:
+                    buscar();
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                default:
+                    throw new AssertionError();
+            }
+
+        } while (opcion != 6);
+    }
+
+    private void listar() {
+        vista.mostrarListaCategorias(dao.listarTodos());
+    }
+
     private void buscar() {
-        int id = vista.solicitarIdCategoria(); 
+        int id = vista.solicitarID();
         Categoria categoria = dao.buscarPorId(id);
-        if (categoria != null ) {
+        if (categoria != null) {
             vista.mostrarCategoria(categoria);
-        }else {
-            vista.mostrarMensaje("Cliente  no encontrado con el ID  " + id); 
+
+        } else {
+            vista.mostrarMensaje("Categoria no encontrado con el Id" + id);
         }
-    
     }
 }
