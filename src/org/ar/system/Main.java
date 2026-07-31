34bb5d58e431dcdaa6aa671bc09c73a32590df17
Andree Zabala <azabala-2023347@kinal.edu.gt>
2026-07-31 10:47:52 -0600

    Actualizar Main.java

--- src/org/ar/system/Main.java
+++ src/org/ar/system/Main.java
@@ -1,15 +1,43 @@
 package org.ar.system;
 
-import org.ar.view.MenuPrincipal;
+import java.io.IOException;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+/**
+ *
+ * @author Alvaro Calderon
+ */
+public class Main extends Application {
+
+    private static Stage escenarioPrincipal;
+
+    public static void cambiarEscena(String rutaFXML) throws IOException {
+        //Parent raiz = FXMLLoader.load(getClass().getResource(rutaFXML));
+        Parent raiz = FXMLLoader.load(Main.class.getResource(rutaFXML));
+        Scene escena = new Scene(raiz);
+        escenarioPrincipal.setScene(escena);
+        escenarioPrincipal.sizeToScene();
+        escenarioPrincipal.centerOnScreen();
+        escenarioPrincipal.show();
+    }
+
+    @Override
+    public void start(Stage escenarioPrincipal) throws Exception {
+        //convertir .fxml en nodo raiz
+        Main.escenarioPrincipal = escenarioPrincipal;
+        escenarioPrincipal.setTitle("Key Librería");
+        //Se arranca directo en la vista de Editorial, sin pedir login.
+        //El login (InicioSesionView) se deja disponible en el proyecto
+        //por si se necesita reactivar mas adelante.
+        cambiarEscena("/org/ar/view/EditorialView.fxml");
+    }
 
     public static void main(String[] args) {
-
-        // MVC Modelo, Vista, Controlador
-        // DAO (Objeto de Acceso a Datos)
-
-        MenuPrincipal menu = new MenuPrincipal();
-        menu.iniciarSistema();
+        launch(args);
     }
 }
