ce41fd8a6a2e386d89cce58df5be9101b380f647
Andree Zabala <azabala-2023347@kinal.edu.gt>
2026-07-31 10:47:46 -0600

    Actualizar modelo editorial.java

--- src/org/ar/model/editorial.java
+++ src/org/ar/model/editorial.java
@@ -1,18 +1,21 @@
 package org.ar.model;
 
 public class editorial {
+
     /*
     create table editorial
-    */
+     */
     //Atributos de clasde
     private String nit;
     private String nombre_editorial;
     private String telefono_editorial;
     private String direccion_editoria;
+
     //Constructores: asignacion de datos, instanciar objetos
     //vacio, lleno, personalizado.
     public editorial() {
     }
+
     //llene 
     public editorial(String nit, String nombre_editorial, String telefono_editorial, String direccion_editoria) {
         this.nit = nit;
@@ -20,38 +23,38 @@ public class editorial {
         this.telefono_editorial = telefono_editorial;
         this.direccion_editoria = direccion_editoria;
     }
-        //getter and setters
- 
+    //getter and setters
+
     public String getNit() {
         return nit;
     }
- 
+
     public void setNit(String nit) {
         this.nit = nit;
     }
- 
+
     public String getNombre_editorial() {
         return nombre_editorial;
     }
- 
-public void setNombre_editorial(String nombre_editorial) {
-    this.nombre_editorial = nombre_editorial.toUpperCase();
-}
- 
+
+    public void setNombre_editorial(String nombre_editorial) {
+        this.nombre_editorial = nombre_editorial.toUpperCase();
+    }
+
     public String getTelefono_editorial() {
         return telefono_editorial;
     }
- 
+
     public void setTelefono_editorial(String telefono_editorial) {
         this.telefono_editorial = telefono_editorial;
     }
- 
+
     public String getDireccion_editoria() {
         return direccion_editoria;
     }
- 
+
     public void setDireccion_editoria(String direccion_editoria) {
         this.direccion_editoria = direccion_editoria;
     }
- 
+
 }
