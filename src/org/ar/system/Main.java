cd330770878b4ac0b247a54146a624f4b13c83f3
Informatica <rpico-2023564@kinal.edu.gt>
2026-08-13 10:13:19 -0600

    se modifico main

--- src/org/ar/system/Main.java
+++ src/org/ar/system/Main.java
@@ -3,8 +3,9 @@ package org.ar.system;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+
 
 public class Main extends Application {
 
