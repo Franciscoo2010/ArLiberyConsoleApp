b5caba7ce82a28702b034064a6182604da992255
Alexis Diéguez <adieguez-2023463@kinal.edu.gt>
2026-08-13 10:56:12 -0600

    feat(ft/clientes) se añadieron cambios en la clase main

--- src/org/ar/system/Main.java
+++ src/org/ar/system/Main.java
@@ -1,14 +1,35 @@
 package org.ar.system;
  
-import org.ar.view.MenuPrincipal;
- 
-public class Main {
-    public static void main(String[] args) {
-        //sout
-        //MVC Modelo,Vista, Controlador. Esto mejora la legibilidad y estandariza en código (patron de diseño)
-        //DAO (objeto de acceso a datos)
-        MenuPrincipal menu = new MenuPrincipal();
-        menu.iniciarSistema();
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    private static Stage escenarioPrincipal;
+
+    @Override
+    public void start(Stage escenarioPrincipal) throws Exception {
+        this.escenarioPrincipal = escenarioPrincipal;
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/ar/view/ClienteView.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        escenarioPrincipal.setTitle("Ar Libery");
+        escenarioPrincipal.setScene(scene);
+        escenarioPrincipal.show();
     }
-    
+
+    public static void cambiarVista(String fxmlPath) throws Exception {
+        FXMLLoader loader = new FXMLLoader(Main.class.getResource(fxmlPath));
+        Parent root = loader.load();
+        escenarioPrincipal.setScene(new Scene(root));
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
