cfadae36bc14445608a0bb6cf6109d9ee760f8d0
Gabriel Chiu <gchiu-2026118@kinal.edu.gt>
2026-08-12 20:12:46 -0600

    feat(ft/autores) se añadieron cambios en la conexión

--- src/org/ar/util/Conexion.java
+++ src/org/ar/util/Conexion.java
@@ -10,6 +10,7 @@ public class Conexion {
     private static final String URL = "jdbc:mysql://localhost:3306/libreriadb_in4cm?serverTimezone=UTC";
     private static final String USER = "IN4CM";
     private static final String PASSWORD = "#NdimAM4";
+ 
     //Constructor privado para evitar que hagan "new Conexion()" fuera de esta clase
     private Conexion() {
         try {
@@ -18,6 +19,7 @@ public class Conexion {
             System.err.println("Error Driver: " + e.getMessage());
         }
     }
+ 
     //Método público estático para obtener la única instancia del Gestor
     public static synchronized Conexion getInstancia() {
         if (instancia == null) {
@@ -25,9 +27,10 @@ public class Conexion {
         }
         return instancia;
     }
+ 
     //Método para entregar una conexión fresca cada vez que se pida
     public Connection conectar() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
- 
+
 }
